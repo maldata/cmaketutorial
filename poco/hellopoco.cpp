e0aaad464cb33401f2dc1b0a6bb1dc6bb5cc73aa
Mark Locascio <maldata@gmail.com>
2017-08-17 13:42:45 -0500

    The sample now uses the Poco Process module

--- poco/hellopoco.cpp
+++ poco/hellopoco.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <Poco/Util/Application.h>
+#include <Poco/Process.h>
+
+// Based on documentation from:
+// https://pocoproject.org/docs/package-Foundation.Processes.html
+// https://pocoproject.org/slides/150-Processes.pdf
 
 class HelloPocoApplication : public Poco::Util::Application
 {
@@ -7,6 +12,18 @@ protected:
   virtual int main(const std::vector<std::string> &args)
   {
     std::cout << "Hello, POCO C++ Libraries!" << std::endl;
+    
+    std::string cmd("/usr/bin/touch");
+    std::vector<std::string> pargs;
+    pargs.push_back("-f");  // Does nothing. Just for the sake of demonstration.
+    pargs.push_back("testfile.txt");
+    std::string dir("/home/dmc");
+    
+    Poco::ProcessHandle ph = Poco::Process::launch(cmd, pargs, dir);
+    
+    std::cout << "Process started. Waiting..." << std::endl;
+    Poco::Process::wait(ph);
+    std::cout << "All done!" << std::endl;
     
     return EXIT_OK;
   }
